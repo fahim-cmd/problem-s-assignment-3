ce0f7e35e19343fe9ca4014dc9cf1710828087bd
fahim-cmd <hfahim087@gmail.com>
2021-01-22 11:41:17 +0600

    processing problems

--- assignment.js
+++ assignment.js
@@ -1,11 +1,16 @@
 
 
 function kilometerToMeter(kilometer) {
-    const meter = kilometer * 1000;  // 1 kilometer = 1000 meter
-    return meter;
+    if(kilometer > 0){
+        const meter = kilometer * 1000;  // 1 kilometer = 1000 meter
+        return meter;
+    }
+    else if (kilometer == undefined || kilometer < 0){
+        return "Your value is undefined";
+    }
 }
-const result = kilometerToMeter(10);
-console.log(result)
+// const result = kilometerToMeter(-1);
+// console.log(result)
 
 
 //How to budget for electronics products?
@@ -16,43 +21,67 @@ function budgetCalculator(watch, phone, laptop) {
     const laptopPrice = laptop * 500;
 
     const totalBudget = watchPrice + phonePrice + laptopPrice;
+   
     return totalBudget;
 }
-// const total = budgetCalculator(2, 5, 2);
+// const total = budgetCalculator(-1, -1, -1);
 // // console.log(total)
 
 
 // How to calculate the hotel bill??
 
 function hotelCost(remaining) {
-    if (remaining <= 10) {
-        const stayingCostFirst = remaining * 100
-        return stayingCostFirst;
-    }
-    if (remaining <= 20) {
-        const stayingCostSecond = (10 * 100) + ((remaining - 10) * 80)  //sum of first and second 10days
-        return stayingCostSecond;
-    }
-    else {
-        const stayingCostThird = (10 * 100) + (10 * 80) + ((remaining - 20) * 50)
-        return stayingCostThird;
-    }
+
+        if( remaining > 0){
+            let rent = 0;
+            if (remaining <= 10) {
+                rent = remaining * 100;
+            }
+            else if (remaining <= 20) {
+                const rentFirst10 = 10 * 100;
+                const stayingDays = remaining - 10;
+                const rentSecond10 = stayingDays * 80;
+                rent = rentFirst10 + rentSecond10;
+            }    
+            else{
+                const rentFirst10 = 10 * 100;
+                const rentSecond10 = 10 * 80;
+                const newStayingDays = remaining - 20;
+                const rentInfinite = newStayingDays * 50;
+                rent = rentFirst10 + rentSecond10 + rentInfinite;
+            }
+            return rent;
+        }
+        else{
+            return "Invalid value";
+        }
+   
 }
-// const output = hotelCost(20);
-// // console.log(output)
+// const output = hotelCost();
+// console.log(output)
 
 
 // how to find a longest name in a array?
 
 function megaFriend(arr) {
-    let longestName = arr[0];
-    for (var i = 0; i < arr.length; i++) {
-        const nameLength = arr[i].length;
-        if (nameLength > longestName.length) {
-            longestName = arr[i];
-        }
+
+    if(arr < 0 || arr == ""){
+        return "Something went wrong: ERROR";
     }
-    return longestName;
+
+    else{
+        let longestName = arr[0];
+   
+        for (var i = 0; i < arr.length; i++) {
+            const nameLength = arr[i].length;
+            if (nameLength > longestName.length) {
+                longestName = arr[i];
+            }       
+        }
+        
+        return longestName;
+    }
 }
-// var friends = megaFriend(["sakib", "mushfiq", "rubel hossain"]);
+// var friends = megaFriend(["sakib", "tamim iqbal", "masrafi bin murtaza"]);
 // console.log(friends);
+
